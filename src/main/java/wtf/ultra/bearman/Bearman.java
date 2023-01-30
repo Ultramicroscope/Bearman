060bc8343b06841d4d22f6e391acafc08913487b
Ryan <bridgeplayer411@gmail.com>
2023-01-30 17:49:06 -0500

    Added comments

--- src/main/java/wtf/ultra/bearman/Bearman.java
+++ src/main/java/wtf/ultra/bearman/Bearman.java
@@ -13,16 +13,18 @@ import org.lwjgl.input.Keyboard;
 @Mod(modid = "bearman", version = "1.8.9")
 public class Bearman {
     private final Minecraft mc = Minecraft.getMinecraft();
-    private int forwardKeyCode;
-    private int forwardState;
+    private int forwardKeyCode,forwardState;
 
     @Mod.EventHandler
     public void init(FMLInitializationEvent event) {
+        // Registering event handlers to the Event Bus
         MinecraftForge.EVENT_BUS.register(this);
     }
 
+    /* Listen to when the sprint key is released. Set forward key state to unpressed */
     @SubscribeEvent
     public void onInput(InputEvent.KeyInputEvent event) {
+        // If this input is the sprint key and that key is being released (key up).
         if (Keyboard.getEventKey() == mc.gameSettings.keyBindSprint.getKeyCode() && !Keyboard.getEventKeyState()) {
             forwardKeyCode = mc.gameSettings.keyBindForward.getKeyCode();
             KeyBinding.setKeyBindState(forwardKeyCode, false);
@@ -30,10 +32,13 @@ public class Bearman {
         }
     }
 
+    /* After forward key was set to unpressed, wait one tick then repress. */
     @SubscribeEvent
     public void onPlayerTick(TickEvent.PlayerTickEvent event) {
+        // Logic to wait a tick then reset.
         if (forwardState == 2) {
             forwardState = 0;
+            // Set forward key state to pressed if it is held down.
             if (Keyboard.isKeyDown(forwardKeyCode)) {
                 KeyBinding.setKeyBindState(forwardKeyCode, true);
             }
